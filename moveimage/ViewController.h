63fae7a54b82cd8bcedf621c45ba240796f48a19
divya136 <sathish@cupertino.local>
2015-02-16 18:51:46 +0530

    Initial Commit

---
+++ moveimage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  moveimage
+//
+//  Created by gunasundari on 16/02/15.
+//  Copyright (c) 2015 XERAGO. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
